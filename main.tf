8bada8fc19c45406cbf15065429557941221a9f1
imlystyi <vladte3c@gmail.com>
2024-04-11 15:40:13 +0300

    feat(lab-3): provide CloudFront

--- main.tf
+++ main.tf
@@ -65,3 +65,12 @@ module "s3" {
   name  = "s3"
   stage = "dev"
 }
+
+module "cloudfront" {
+  source = "./cloudfront"
+
+  name   = "cloudfront"
+  stage  = "dev"
+
+  bucket_regional_domain_name = module.s3.bucket_regional_domain_name
+}
